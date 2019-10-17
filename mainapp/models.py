64f9b0dbd532984a424b4eb022764e810c176d9a
Srinivas Reddy Danda <srinivas.d@SLS.ads.valuelabs.net>
2019-10-17 12:44:23 +0530

    Initial commit

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,12 +5,12 @@ from django.db import models
 class InputImages(models.Model):
     comparison_set_name = models.CharField(max_length=100)
     input_image = models.ImageField(
-        upload_to='/path/InputImages',
+        upload_to='your_path',
         max_length=254,
         blank=True,
         null=True)
     standard_image = models.ImageField(
-        upload_to='/path/StandardImages',
+        upload_to='your_path',
         max_length=254,
         blank=True,
         null=True)
@@ -22,7 +22,7 @@ class InputImages(models.Model):
 class OutputImage(models.Model):
     input_key = models.ForeignKey(InputImages, on_delete=models.CASCADE)
     output_image = models.ImageField(
-        upload_to='/path/OutputImages',
+        upload_to='your_path',
         max_length=254,
         blank=True,
         null=True)
--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.urls import path, include
 
 from mainapp.views import *
 
